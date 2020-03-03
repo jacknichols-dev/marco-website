2198e5181f80cb7d4778c7fc2b31d2319e836350
Jack Nichols <jacknichols@jacks-mbp.home>
2020-03-03 12:15:01 +0000

    updates

--- js/carousel.js
+++ js/carousel.js
@@ -59,6 +59,21 @@ if (auto) {
 
 //review carousel
 
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("review-carousel__slide");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 5000); // Change image every 2 seconds
+}
+
 /*
 //set the slide and image variables
 
